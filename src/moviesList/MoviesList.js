ace3ab7aba9a911c523a79d8fe7baf9ada57afb2
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-19 14:21:26 +0100

    implement expandable movie lists for tempMovieData and tempWatchedData

--- src/moviesList/MoviesList.js
+++ src/moviesList/MoviesList.js
@@ -1,18 +1,82 @@
+import { useState } from "react";
 import { tempMovieData } from "../tempMovieData";
+import { tempWatchedData } from "../tempWatchedData";
+import {
+  Button,
+  Container,
+  List,
+  ListItem,
+  MovieDescription,
+  Movies,
+  MoviesContainer,
+} from "./styled";
 
 function MoviesList() {
+  const [open, setOpen] = useState(true);
+  const [open2, setOpen2] = useState(true);
+  const onListOpen = () => {
+    setOpen((open) => !open);
+  };
+  const onListOpen2 = () => {
+    setOpen2((open2) => !open2);
+  };
   return (
-    <div>
-      {tempMovieData.map((movie) => {
-        return (
-          <li key={movie.imdbID}>
-            {movie.Title}
-            {movie.Year}
-            <img src={movie.Poster} alt="movie.Title" />
-          </li>
-        );
-      })}
-    </div>
+    <Container>
+      <Movies>
+        <MoviesContainer>
+          {" "}
+          <Button onClick={onListOpen}>{open ? "Close" : "Open"}</Button>
+          <List $open={open} style={{ display: `${open ? "block" : "none"}` }}>
+            {tempMovieData.map((movie) => {
+              return (
+                <ListItem key={movie.imdbID}>
+                  {" "}
+                  <img
+                    style={{ width: "100px" }}
+                    w
+                    src={movie.Poster}
+                    alt="movie.Title"
+                  />
+                  <MovieDescription>
+                    <span>{movie.Title}</span>
+                    <span>{movie.Year}</span>
+                  </MovieDescription>
+                </ListItem>
+              );
+            })}
+          </List>
+        </MoviesContainer>{" "}
+      </Movies>
+
+      <Movies>
+        <MoviesContainer>
+          {" "}
+          <Button onClick={onListOpen2}>{open2 ? "Close" : "Open"}</Button>
+          <List
+            $open2={open2}
+            style={{ display: `${open2 ? "block" : "none"}` }}
+          >
+            {tempWatchedData.map((movie) => {
+              return (
+                <ListItem key={movie.imdbID}>
+                  {" "}
+                  <img
+                    style={{ width: "100px" }}
+                    w
+                    src={movie.Poster}
+                    alt="movie.Title"
+                  />
+                  <MovieDescription>
+                    <span>{movie.Title}</span>
+                    <span>{movie.Year}</span>
+                  </MovieDescription>
+                </ListItem>
+              );
+            })}
+          </List>
+        </MoviesContainer>{" "}
+      </Movies>
+    </Container>
   );
 }
 
