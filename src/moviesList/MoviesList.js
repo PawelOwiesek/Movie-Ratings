477019f3efd5954eed4dd33b8e5655388c4e5e39
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-19 09:33:34 +0100

    render movie list using tempMovieData in MoviesList component

--- src/moviesList/MoviesList.js
+++ src/moviesList/MoviesList.js
@@ -1,5 +1,19 @@
+import { tempMovieData } from "../tempMovieData";
+
 function MoviesList() {
-  return <div></div>;
+  return (
+    <div>
+      {tempMovieData.map((movie) => {
+        return (
+          <li key={movie.imdbID}>
+            {movie.Title}
+            {movie.Year}
+            <img src={movie.Poster} alt="movie.Title" />
+          </li>
+        );
+      })}
+    </div>
+  );
 }
 
 export default MoviesList;
