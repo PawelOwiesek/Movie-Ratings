5583b1eaa0f5832d3eb5822b340cbe46facc8e40
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-19 17:30:40 +0100

    add styled component for MoviesContainer to enhance layout and responsiveness

---
+++ src/moviesListsContainer/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MoviesContainer = styled.div`
+  padding: 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: 1600px;
+  gap: 40px;
+  margin: 0 auto;
+`;
