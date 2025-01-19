25a51e2ba250dcb1cc33cd39f0786511419875c7
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-19 17:30:50 +0100

    refactor NavBar component to accept children for improved flexibility

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,16 +1,7 @@
-import { LogoContainer, NavContainer, Results, Search } from "./styled";
+import { NavContainer } from "./styled";
 
-function NavBar() {
-  return (
-    <NavContainer>
-      <LogoContainer>
-        <img style={{ width: "40px" }} src="/images/favicon.svg" alt="logo" />
-        <h2>Movies Ratings</h2>
-      </LogoContainer>
-      <Search type="text" placeholder="Search movie..." />
-      <Results>Found X results</Results>
-    </NavContainer>
-  );
+function NavBar({ children }) {
+  return <NavContainer>{children}</NavContainer>;
 }
 
 export default NavBar;
