9ed9e96ea896dc3e8554d1a17c5c3ddcaa5bec5e
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-19 09:22:41 +0100

    add NavBar and MoviesList components to App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import MoviesList from "./moviesList/MoviesList";
+import NavBar from "./navbar/NavBar";
+
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <NavBar />
+      <MoviesList />
+    </div>
+  );
 }
