3aea3d241b74c654987520b034dd9e677247e0bf
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-19 17:30:28 +0100

    add MoviesListsContainer component to wrap movie lists with styled container

---
+++ src/moviesListsContainer/MoviesListsContainer.js
@@ -0,0 +1,7 @@
+import { MoviesContainer } from "./styled";
+
+function MoviesListsContainer({ children }) {
+  return <MoviesContainer>{children}</MoviesContainer>;
+}
+
+export default MoviesListsContainer;
