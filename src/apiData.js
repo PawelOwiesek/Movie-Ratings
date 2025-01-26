fdd0feb44a07345002443de346b9738584d7c0ec
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-26 23:02:23 +0100

    enhance API function to handle errors and manage loading state

--- src/apiData.js
+++ src/apiData.js
@@ -1,11 +1,21 @@
 const KEY = 32389744;
 
-export const API = ({ setMovies, query, setIsLoading }) => {
-  setIsLoading(true);
-  setTimeout(async () => {
-    const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-    const data = await res.json();
-    setMovies(data.Search);
-    setIsLoading(false);
-  }, 3000);
+export const API = ({ setMovies, query, setIsLoading, setError }) => {
+  try {
+    setIsLoading(true);
+    setTimeout(async () => {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      if (!res.ok) {
+        setError(true);
+      }
+
+      const data = await res.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+    }, 3000);
+  } catch (err) {
+    console.error(err.message);
+  }
 };
