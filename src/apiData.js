09c2b8daf525716996b7f3470f5aaf3999e4dc3f
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-26 22:03:13 +0100

    feat: add loading state management to API call in apiData.js

--- src/apiData.js
+++ src/apiData.js
@@ -1,7 +1,11 @@
 const KEY = 32389744;
 
-export const API = async ({ setMovies, query }) => {
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-  const data = await res.json();
-  setMovies(data.Search);
+export const API = ({ setMovies, query, setIsLoading }) => {
+  setIsLoading(true);
+  setTimeout(async () => {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+    const data = await res.json();
+    setMovies(data.Search);
+    setIsLoading(false);
+  }, 3000);
 };
