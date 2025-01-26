845f38ca2957e92e5a04535bb4d5b6b49b8e4f07
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-26 21:02:25 +0100

    feat: enhance SearchComponent to manage query state and handle input changes

--- src/navbar/SearchComponent.js
+++ src/navbar/SearchComponent.js
@@ -1,7 +1,16 @@
 import { Search } from "./styled";
 
-function SearchComponent() {
-  return <Search type="text" placeholder="Search movie..." />;
+function SearchComponent({ setQuery, query }) {
+  return (
+    <Search
+      value={query}
+      onChange={(e) => {
+        setQuery(e.target.value);
+      }}
+      type="text"
+      placeholder="Search movie..."
+    />
+  );
 }
 
 export default SearchComponent;
