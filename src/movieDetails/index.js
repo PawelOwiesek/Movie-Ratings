0c7928407860ca282d6b598fa4de888dc538a5a6
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-01 13:31:06 +0100

    fetch movie details using API and display selected movie information

--- src/movieDetails/index.js
+++ src/movieDetails/index.js
@@ -1,20 +1,54 @@
-import { useEffect } from "react";
-import { fetchMovieDetails } from "../apiData";
+import { useEffect, useState } from "react";
 import { Movies } from "../moviesList/styled";
+import { KEY } from "../apiData";
 
 export const MovieDetails = ({ selectedId, handleCloseMovie }) => {
+  const [selectedMovie, setSelectedMovie] = useState("");
+
   useEffect(() => {
-    // fetchMovieDetails(selectedId);
-  });
+    const fetchMovieDetails = async () => {
+      const res = await fetch(
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+
+      const data = await res.json();
+      setSelectedMovie(data);
+    };
+    fetchMovieDetails();
+  }, [selectedId]);
+
   return (
-    <Movies>
-      <button
-        style={{ width: "50px", height: "50px" }}
-        onClick={handleCloseMovie}
-      >
-        &larr;
-      </button>
-      <p style={{ color: "#fff" }}>{selectedId}</p>{" "}
+    <Movies style={{ position: "fixed", maxWidth: "800px", right: "120px" }}>
+      <div style={{ padding: "10px", display: "flex" }}>
+        <button
+          style={{
+            width: "50px",
+            height: "50px",
+            borderRadius: "100%",
+            margin: "5px",
+            fontSize: "30px",
+          }}
+          onClick={handleCloseMovie}
+        >
+          &larr;
+        </button>
+        <img
+          style={{ width: "275px", height: "275px", margin: "20px" }}
+          src={selectedMovie.Poster}
+          alt={selectedMovie.Title}
+        />
+        <div>
+          <p style={{ color: "#fff" }}>{selectedMovie.Title}</p>
+          <p style={{ color: "#fff" }}>Runtime: {selectedMovie.Runtime}</p>
+          <p style={{ color: "#fff" }}>Director: {selectedMovie.Director}</p>
+          <p style={{ color: "#fff" }}>Genre: {selectedMovie.Genre}</p>
+          <p style={{ color: "#fff" }}>
+            imdbRating: {selectedMovie.imdbRating}
+          </p>
+          <p style={{ color: "#fff" }}>imdbVotes: {selectedMovie.imdbVotes}</p>
+          <p style={{ color: "#fff" }}> {selectedMovie.Plot}</p>
+        </div>
+      </div>
     </Movies>
   );
 };
