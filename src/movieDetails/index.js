89a65b6d0bc0041783e4650d4ca51344fa2af209
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-02 15:45:19 +0100

    add loading state to MovieDetails component

--- src/movieDetails/index.js
+++ src/movieDetails/index.js
@@ -11,6 +11,7 @@ import {
   MovieTitle,
 } from "./styled";
 import { Stars } from "../star/starComponent";
+import { Loading } from "../asideActions/loader/loader";
 
 export const MovieDetails = ({
   selectedId,
@@ -21,49 +22,58 @@ export const MovieDetails = ({
   setRating,
 }) => {
   const [selectedMovie, setSelectedMovie] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
+      setIsLoading(true);
       const res = await fetch(
         `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
 
       const data = await res.json();
-      console.log(data);
       setSelectedMovie(data);
     };
     fetchMovieDetails();
+    setIsLoading(false);
   }, [selectedId]);
 
   return (
     <Movies>
-      {" "}
-      <Button onClick={handleCloseMovie}>&larr;</Button>{" "}
-      <FlexContainer>
-        <Image src={selectedMovie.Poster} alt={selectedMovie.Title} />
-        <Description>
-          <MovieTitle>{selectedMovie.Title}</MovieTitle>
-          <p>Runtime: {selectedMovie.Runtime}</p>
-          <p>Director: {selectedMovie.Director}</p>
-          <Genre>
-            {selectedMovie.Genre?.split(",").map((genre) => (
-              <GenreItem>{genre.trim()}</GenreItem>
-            ))}
-          </Genre>
-          <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
-            <Stars
-              hover={hover}
-              setHover={setHover}
-              rating={rating}
-              setRating={setRating}
-            />
-            Rating {hover || rating || ""}
-          </div>
-          <p>imdbRating: {selectedMovie.imdbRating}</p>
-          <p>imdbVotes: {selectedMovie.imdbVotes}</p>
-          <p> {selectedMovie.Plot}</p>
-        </Description>
-      </FlexContainer>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Button onClick={handleCloseMovie}>&larr;</Button>
+          <FlexContainer>
+            <Image src={selectedMovie.Poster} alt={selectedMovie.Title} />
+            <Description>
+              <MovieTitle>{selectedMovie.Title}</MovieTitle>
+              <p>Runtime: {selectedMovie.Runtime}</p>
+              <p>Director: {selectedMovie.Director}</p>
+              <Genre>
+                {selectedMovie.Genre?.split(",").map((genre) => (
+                  <GenreItem>{genre.trim()}</GenreItem>
+                ))}
+              </Genre>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "20px" }}
+              >
+                <Stars
+                  hover={hover}
+                  setHover={setHover}
+                  rating={rating}
+                  setRating={setRating}
+                />
+                Rating {hover || rating || ""}
+              </div>
+              <p>imdbRating: {selectedMovie.imdbRating}</p>
+              <p>imdbVotes: {selectedMovie.imdbVotes}</p>
+              <p> {selectedMovie.Plot}</p>
+            </Description>
+          </FlexContainer>
+        </>
+      )}
     </Movies>
   );
 };
