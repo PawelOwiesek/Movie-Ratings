4ba04e9570b2354993b9d9cd9bf660079b22f2e1
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-02 13:38:50 +0100

    add styled components for MovieDetails layout and design

---
+++ src/movieDetails/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Movies = styled.div`
+  position: fixed;
+  max-width: 800px;
+  right: 120px;
+  background-color: #222121;
+  border: 2px solid #ffd700;
+  border-radius: 25px;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  padding: 10px;
+`;
+
+export const Description = styled.div`
+  color: #ffffff;
+  padding-top: 10px;
+  font-size: 20px;
+`;
+export const Button = styled.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  margin: 5px;
+  font-size: 30px;
+`;
+
+export const Image = styled.img`
+  width: 275px;
+  height: 275px;
+  margin: 20px;
+`;
