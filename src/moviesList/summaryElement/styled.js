616e45cb6d555dead8dde2c7c9c0f4f056dad7e2
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-16 15:59:10 +0100

    enhance responsive styles for title and rating container in movie list

--- src/moviesList/summaryElement/styled.js
+++ src/moviesList/summaryElement/styled.js
@@ -1,10 +1,20 @@
 import styled from "styled-components";
 
-export const Title = styled.h2``;
+export const Title = styled.h2`
+  @media (max-width: 1024px) {
+    font-size: 18px;
+  }
+`;
 
 export const RatingContainer = styled.div`
   display: flex;
   gap: 110px;
   align-items: center;
   font-size: 22px;
+
+  @media (max-width: 1024px) {
+    gap: 20px;
+    max-width: 300px;
+    font-size: 16px;
+  }
 `;
